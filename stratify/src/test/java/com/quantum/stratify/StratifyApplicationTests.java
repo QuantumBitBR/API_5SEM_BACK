bc7ec032377676de8c8d5cf8469f8ec67312713f
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2025-03-07 19:21:33 -0300

    chore(init) - scrum-30: init base project

---
+++ stratify/src/test/java/com/quantum/stratify/StratifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantum.stratify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StratifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
