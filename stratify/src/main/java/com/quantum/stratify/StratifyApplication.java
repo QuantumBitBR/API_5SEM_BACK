2276d30b1dfca5980592e3cbb44e6e5f96e241b9
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2025-03-08 22:13:53 -0300

    chore(project) - scrum-30: add database connection

--- stratify/src/main/java/com/quantum/stratify/StratifyApplication.java
+++ stratify/src/main/java/com/quantum/stratify/StratifyApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StratifyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StratifyApplication.class, args);
 	}
-
 }
