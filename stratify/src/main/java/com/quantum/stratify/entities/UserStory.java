d2740f599d1699deec94d1fc6035007aa82240e1
AndreMeneses0103 <andremeneses0103@gmail.com>
2025-03-13 17:22:31 -0300

    feat(entity): criada nova entidade UserStory

---
+++ stratify/src/main/java/com/quantum/stratify/entities/UserStory.java
@@ -0,0 +1,35 @@
+package com.quantum.stratify.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "dim_user_story")
+public class UserStory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_var")
+    private DimVariable variable;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_time")
+    private DimTime time;
+}
