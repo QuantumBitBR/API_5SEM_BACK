1b06bf6672cbb74923fe40497fbebde8246946b0
AndreMeneses0103 <andremeneses0103@gmail.com>
2025-03-13 19:48:45 -0300

    fix(path): caminho das classes corrigido

--- stratify/src/main/java/com/quantum/stratify/entities/DimTime.java
+++ stratify/src/main/java/com/quantum/stratify/entities/DimTime.java
@@ -7,16 +7,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+
+@Entity
+@Table(name = "dim_time")
 public class DimTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- stratify/src/main/java/com/quantum/stratify/repositories/CardRepository.java
+++ stratify/src/main/java/com/quantum/stratify/repositories/CardRepository.java
@@ -1,4 +1,4 @@
-package com.repositories;
+package com.quantum.stratify.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
