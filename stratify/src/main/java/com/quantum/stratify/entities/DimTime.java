77f912cb44c4d84798da90da9e29b460e6fba378
AndreMeneses0103 <andremeneses0103@gmail.com>
2025-03-13 14:48:34 -0300

    feat(entity): criada nova entidade DimTime

---
+++ stratify/src/main/java/com/quantum/stratify/entities/DimTime.java
@@ -0,0 +1,33 @@
+package com.quantum.stratify.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DimTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "due_date")
+    private LocalDateTime dueDate;
+
+    @Column(name = "start_date")
+    private LocalDateTime startDate;
+
+    @Column(name = "finish_date")
+    private LocalDateTime finishDate;
+}
