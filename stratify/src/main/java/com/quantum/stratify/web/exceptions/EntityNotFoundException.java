e9bb99ccf4e34346915b890247b06dee835765a9
AndreMeneses0103 <andremeneses0103@gmail.com>
2025-03-13 19:47:51 -0300

    feat(exception): criado novo exception entitynotfoundexception

---
+++ stratify/src/main/java/com/quantum/stratify/web/exceptions/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.quantum.stratify.web.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+    public EntityNotFoundException(String message){
+        super(message);
+    }
+    
+}
