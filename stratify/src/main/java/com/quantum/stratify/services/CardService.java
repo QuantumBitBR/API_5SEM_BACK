c45fb7ae0f3c9becfbe4997001446de68dab9329
Kainanthyz <santos.blanzgamer@gmail.com>
2025-03-14 19:50:57 -0300

    feat(cardsTotal): adiciona endpoint para a busca de quantidade total de cards

--- stratify/src/main/java/com/quantum/stratify/services/CardService.java
+++ stratify/src/main/java/com/quantum/stratify/services/CardService.java
@@ -4,10 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.quantum.stratify.repositories.CardRepository;
+import com.quantum.stratify.repositories.UserStoryRepository;
 
 @Service
 public class CardService {
 
     @Autowired
     private CardRepository cardRepository;
+
+    @Autowired
+    private UserStoryRepository UserStoryRepository;
+
+    public long getTotalCards() {
+        return UserStoryRepository.count();
+    }
 }
--- stratify/src/main/java/com/quantum/stratify/web/controllers/CardController.java
+++ stratify/src/main/java/com/quantum/stratify/web/controllers/CardController.java
@@ -1,12 +1,23 @@
 package com.quantum.stratify.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.quantum.stratify.services.CardService;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/cards")
 public class CardController {
 
+    @Autowired
+    private CardService cardService;
+
+    @GetMapping("/total")
+    public long getTotalCards() {
+        return cardService.getTotalCards();
+    }
 }
