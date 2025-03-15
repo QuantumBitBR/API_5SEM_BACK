69ed13ccb22466035017cb811e4e5a889bfb4059
Kainanthyz <santos.blanzgamer@gmail.com>
2025-03-14 21:34:06 -0300

    docs: adição do swagger no endpoint  de totalCards

--- stratify/src/main/java/com/quantum/stratify/web/controllers/CardController.java
+++ stratify/src/main/java/com/quantum/stratify/web/controllers/CardController.java
@@ -8,15 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.quantum.stratify.services.CardService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/cards")
+@Tag(name = "Cards", description = "Endpoints para gerenciamento de cards")
 public class CardController {
 
     @Autowired
     private CardService cardService;
 
     @GetMapping("/total")
+    @Operation(
+        summary = "Retorna a quantidade total de cards",
+        description = "Este endpoint retorna o número total de cards cadastrados no sistema.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Quantidade total de cards retornada com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+        }
+    )
     public long getTotalCards() {
         return cardService.getTotalCards();
     }
