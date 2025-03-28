9407b0dfca92936fa5892c167dd1ea65849565ae
Kainanthyz <santos.blanzgamer@gmail.com>
2025-03-28 17:22:34 -0300

    SCRUM-29-feat(CardsPorPeriodo): Adding Swagger to UserStoryService and implementing tests for the CardsPorPeriodo method

--- stratify/src/main/java/com/quantum/stratify/services/UserStoryService.java
+++ stratify/src/main/java/com/quantum/stratify/services/UserStoryService.java
@@ -3,6 +3,11 @@ package com.quantum.stratify.services;
 import com.quantum.stratify.repositories.UserStoryRepository;
 import com.quantum.stratify.web.dtos.ResponseQuantidadeCardsByPeriodo;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
@@ -13,6 +18,18 @@ public class UserStoryService {
     @Autowired
     private UserStoryRepository userStoryRepository;
     
+    @Operation(
+        summary = "Count user stories by period",
+        description = "Returns the count of created and finished user stories within the specified date range",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Successful operation",
+                content = @Content(schema = @Schema(implementation = ResponseQuantidadeCardsByPeriodo.class))),
+            @ApiResponse(
+                responseCode = "400",
+                description = "Invalid date range provided"
+            )})
     public ResponseQuantidadeCardsByPeriodo countUserStoriesByPeriod(LocalDateTime dataInicio, LocalDateTime dataFim) {
         Long criadas = userStoryRepository.countCriadasNoPeriodo(dataInicio, dataFim);
         Long finalizadas = userStoryRepository.countFinalizadasNoPeriodo(dataInicio, dataFim);
