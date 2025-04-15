a64dca76c1c0a1cb4d937ac0ced731532a32f829
Kainanthyz <santos.blanzgamer@gmail.com>
2025-04-14 22:18:13 -0300

    SCRUM-81(feat): finishing resolving merge conflicts with sprint-2

--- stratify/src/main/java/com/quantum/stratify/services/UsuarioService.java
+++ stratify/src/main/java/com/quantum/stratify/services/UsuarioService.java
@@ -3,6 +3,9 @@ package com.quantum.stratify.services;
 import com.quantum.stratify.entities.Usuario;
 import com.quantum.stratify.repositories.UsuarioRepository;
 import jakarta.persistence.EntityNotFoundException;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
